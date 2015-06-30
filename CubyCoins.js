7902f4f7a97efd5a113a3b1e74d81a03689ca7c4
DevillordMCPE <maulaxe@yahoo.com>
2015-06-30 21:18:58 +0700

    Update CubyCoins.js

--- CubyCoins.js
+++ CubyCoins.js
@@ -2,3 +2,63 @@
   RPG Mod Pack- Cuby Coins
   Creator : REDevilLord & respectZ
 */
+
+var copperCuby;
+var silverCuby;
+var goldCuby;
+
+function deathHook(m,v) {
+  if(m == getPlayerEnt()) {
+    copperCuby = copperCuby + Math.floor(Math.random()*(100));
+    var dd = Math.floor(Math.random()*(10));
+    if(dd <= 7) {
+      silverCuby++;
+    }
+    if(dd > 7) {
+      goldCuby++;
+    }
+    if(copperCuby >= 100) {
+      copperCuby = copperCuby - 100;
+      silverCuby = silverCuby + 1;
+    }
+    if(silverCuby >= 100) {
+      silverCuby = silverCuby - 100;
+      goldCuby = goldCuby + 1;
+    }
+  }
+}
+
+function destroyBlock(x,y,z,side) {
+  var d = getTile(x,y,z) {
+    if(d == 1 || d == 4 || d == 14 || d == 15 || d == 16 || d == 56) {
+      copperCuby = copperCuby + Math.floor(Math.random()*(100));
+      var dd = Math.floor(Math.random()*(10));
+      if(dd <= 7) {
+        silverCuby++;
+      }
+      if(dd > 7) {
+        goldCuby++;
+      }
+      if(copperCuby >= 100) {
+        copperCuby = copperCuby - 100;
+        silverCuby = silverCuby + 1;
+      }
+      if(silverCuby >= 100) {
+        silverCuby = silverCuby - 100;
+        goldCuby = goldCuby + 1;
+      }
+    }
+  }
+}
+
+function procCmd(cmd) {
+  var c = cmd.split(" ");
+  if(cmd == "shop") {
+    if(!c[1]) {
+      clientMessage(" /shop buy <number> <amount");
+      clientMessage(" /shop buy 1 5");
+      clientMessage("Shop 1/69");
+      clientMessage("1.Apple 20 Copper");
+    }
+  }
+}
